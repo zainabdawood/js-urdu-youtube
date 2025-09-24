748f0c6d1eaf0d376f134424d3e21f77caa22477
Zainab Dawood <zainab.dawood11b@gmail.com>
2025-09-24 16:26:42 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";   // treat all js code as newer version
+// alert(3+3)      // we are using js, not browser
+console.log(3
+    +3
+)  // code readability should be high
+console.log("Zainab")
+
+let name ="zainab"  // string datatypes
+let age=18          // number
+let isLoggedIn = false  // boolean 
+
+// number => 2 to power 53
+// bigInt
+// string = ""
+// boolean true/false
+// null => standolone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log( typeof null); // object
+console.log( typeof undefined);// undefined
+
