1eb80adad7207d4703f3a04c2c4208bedd86d442
Apex Space <apexspace.net@gmail.com>
2025-10-03 04:54:09 -0700

    Compariosn Conversion Operation

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,14 @@
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 > 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1); // do differnt type kai datatype ko compare ni krtai
+// console.log(null > 0);
+// // console.log(null >= 0);  // in this case null convert into a number
+// console.log(null == 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
