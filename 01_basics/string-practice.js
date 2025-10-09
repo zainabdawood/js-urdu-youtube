552a79b642f1994aea4a3086a4ffb89743964eb3
Apex Space <apexspace.net@gmail.com>
2025-10-09 02:27:23 -0700

    string practice

---
+++ 01_basics/string-practice.js
@@ -0,0 +1,27 @@
+const name = "zainab"; //Ek variable name banao jisme tumhara naam store ho,
+console.log(`My name is ${name}`);
+
+let item = "book";
+let price = 450;
+console.log(`The price of ${item} is ${price}`);
+
+// let firstName = "Fatima";
+// let lastName = "Ali";
+// let age = 19;
+// console.log(`My name is ${firstName} ${lastName} and my age is ${age}`);
+
+let num = 8;
+console.log(`The double of ${8} is ${num * 2}`);
+
+let subject = "JavaScript";
+s;
+let difficulty = "easy";
+console.log(`I think learning ${subject} is ${difficulty} !`);
+
+// let firstName = "Zainab";
+// let score = 95;
+// console.log(`Congratulation ${firstName}!You secured ${score}% in your exam`);
+
+let firstName = "Zainab";
+let score = 95;
+console.log(`Congratulation ${firstName}! You secured ${score}% in your exam.`);
